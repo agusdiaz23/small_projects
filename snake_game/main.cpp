8a5231ab1e95ada0a3ec5f83587e3cbb0eb60258
RTowari <federico.alonso.redes@gmail.com>
2024-06-08 16:01:49 -0300

    movement update
    now snake will move two cells at a time

--- snake_game/main.cpp
+++ snake_game/main.cpp
@@ -1,10 +1,8 @@
 #include <string>
 #include <cstdlib>
 #include <time.h> 
-//v_0.1
-// Generates array of 6x6 with  blank spaces in each slot
-// Makes function for printing out array, for doing so at will
-// Moves 0 (snake's head) troughout the array. Leaves trace behind
+//v_0.2
+// Snake will be two cells long, and It will move in the array, taking different paths
 
 int print_array(char main_array[][6]) { // Summon function at will to print it out
     for (int i=0; i<6; i++)  {
@@ -18,62 +16,74 @@ int print_array(char main_array[][6]) { // Summon function at will to print it o
     return 0;
 }
 
-int defines_head_position(char main_array[][6]) {
-    int temp_num, pos1, pos2, ant_pos1, ant_pos2;
 
-    // Looks for snake's head position. It's necessary of the logic of the function
+int do_snake_movement(char main_array[][6]) {
+    int temp_num, pos_1, pos_2, ant_pos_1, ant_pos_2;
+
+    // Busco la cabeza de la serpiente
     for (int i=0; i<6; i++)  {
         for (int u=0; u<6; u++)  {
             if(main_array[i][u]=='0') {
-                pos1=i;
-                pos2=u;
+                pos_1=i;
+                pos_2=u;
             }
         }
     }
+    // Dejo guardada la posicion de la cabeza
+    ant_pos_1 = pos_1;
+    ant_pos_2 = pos_2;
 
-    // If my random number is 0, then It'll move between columns, by changing only one of the coords.
-    ant_pos1=pos1;
-    ant_pos2=pos2;
-    while(main_array[pos1][pos2] != ' ' ) { 
-        //This while, will look for empty cells. Once it finds one, It will put the symbol on it.
+
+    //This while will look for empty cells. Once it finds one, It will put the symbol on it.
+    while(main_array[pos_1][pos_2] != ' ' ) { 
         temp_num=rand()%2;
         if (temp_num==0) { 
-            temp_num=rand()%2;
             // If snake's head in the column cero or 5, move it to the right or to the left. If not in 0, do the same, but to any neighboring cell
-            if(pos1==0)
-                pos1++;
-            else if(pos1==5)
-                pos1--;
+            if(pos_1==0)
+                pos_1++;
+            else if(pos_1==5)
+                pos_1--;
             else {
                 temp_num=rand()%2;
                 if(temp_num==0)
-                    pos1++;
+                    pos_1++;
                 else
-                    pos1--;
+                    pos_1--;
             }
         }
         // In this case, It will move in between rows under that rules commented at the if
         else {
-            temp_num=rand()%2;
-            if(pos2==0)
-                pos2++;
-            else if(pos2==5)
-                pos2--;
+            if(pos_2==0)
+                pos_2++;
+            else if(pos_2==5)
+                pos_2--;
             else {
                 temp_num=rand()%2;
                 if(temp_num==0)
-                    pos2++;
+                    pos_2++;
                 else   
-                    pos2--;
+                    pos_2--;
             }
+        }
+
+        if (main_array[pos_1][pos_2] != ' ') {
+            pos_1 = ant_pos_1;
+            pos_2 = ant_pos_2;
+        }
+    }
+    main_array[pos_1][pos_2]='0'; // Pongo la cabeza en la nueva posicion
+
+    for (int i=0; i<6; i++)  { // Busco la cola de la serpiente, para eliminarla antes de moverla, así no se repite
+        for (int u=0; u<6; u++)  {
+            if(main_array[i][u]=='1') {
+                main_array[i][u]=' ';
+            }
+
         }
     }
 
-    // Where the head was, It puts a tail in instead
-    main_array[ant_pos1][ant_pos2] = '#';
-
-    main_array[pos1][pos2]='0';
-
+    main_array[ant_pos_1][ant_pos_2]='1';
+    
     return 0;
 }
 
@@ -82,7 +92,7 @@ int defines_head_position(char main_array[][6]) {
 int main() {
     srand (time(NULL));
     char main_array[6][6];
-    int pos1, pos2;
+    int pos_1, pos_2;
 
     for (int i=0; i<6; i++)  { // Puts empty spaces in array
         for (int u=0; u<6; u++)  {
@@ -91,16 +101,22 @@ int main() {
     }
 
     // Defines started position for the snake's head
-    pos1=rand()%6;   
-    pos2=rand()%6;
-    main_array[pos1][pos2]='0';
-    print_array(main_array);
+    pos_1=rand()%6;   
+    pos_2=rand()%6;
+    main_array[pos_1][pos_2]='0';
 
-    int count = 0;
-    while(count < 5) {
-        defines_head_position(main_array);
+    //Moves snake two blocks
+    for (int u=0;u<4;u++) {
+        for(int i=0; i<2;i++) {
+            do_snake_movement(main_array);
+        }
         print_array(main_array);
-        count++;
     }
+
+
+    // Ahora tengo la serpiente en posicion.
+    
+        
+
 
 }
