2b14674d1834bfa786f5dd9914813979ac65f7fe
RTowari <federico.alonso.redes@gmail.com>
2024-06-08 17:26:53 -0300

    working movement
    Totally integrated movement for the snake, be using a system based on letter and number. It can be changed by the user

--- snake_game/main.cpp
+++ snake_game/main.cpp
@@ -1,9 +1,10 @@
 #include <string>
 #include <cstdlib>
 #include <time.h> 
-//v_0.2
-// Snake will be two cells long, and It will move in the array, taking different paths
+//v_0.3
+// Snake will be tree cells long, and It will move in the array, taking different paths
 
+// Una funcion que hice para imprimir la array a voluntad, asi no tengo que poner un for loop cada vez
 int print_array(char main_array[][6]) { // Summon function at will to print it out
     for (int i=0; i<6; i++)  {
         for (int u=0; u<6; u++)  {
@@ -16,29 +17,96 @@ int print_array(char main_array[][6]) { // Summon function at will to print it o
     return 0;
 }
 
+// Hace aparecer la serpiente
+int do_spawn_snake(char main_array[][6]) {
+    int temp_num, pos_1, pos_2, ant_pos_1, ant_pos_2, position_name=0;
+    
+    pos_1=rand()%6;   
+    pos_2=rand()%6;
+    main_array[pos_1][pos_2]='0';
 
+    
+
+    //Este while busca posiciones vecinas a mi posicion cargada
+    for(int i = 0; i < 2;i++) {
+        ant_pos_1=pos_1; // Esta es la posicion original de mi posicion, guardada para recuperarla bajo ciertas condiciones
+        ant_pos_2=pos_2;
+
+        while(main_array[pos_1][pos_2] != ' ' ) { 
+            temp_num=rand()%2;
+            if (temp_num==0) { 
+                if(pos_1==0)
+                    pos_1++;
+                else if(pos_1==5)
+                    pos_1--;
+                else {
+                    temp_num=rand()%2;
+                    if(temp_num==0)
+                        pos_1++;
+                    else
+                        pos_1--;
+                }
+            }
+            else {
+                if(pos_2==0)
+                    pos_2++;
+                else if(pos_2==5)
+                    pos_2--;
+                else {
+                    temp_num=rand()%2;
+                    if(temp_num==0)
+                        pos_2++;
+                    else   
+                        pos_2--;
+                }
+            }
+
+            if (main_array[pos_1][pos_2] != ' ') {
+                pos_1 = ant_pos_1;
+                pos_2 = ant_pos_2;
+            }
+        }
+        position_name++;
+        main_array[pos_1][pos_2]='0' + position_name;
+    }
+    
+    
+    return 0;
+}
+
+// Una funcion para tomar un char no repetido de una array 2d, y reeplazarlo por la letra que el usuario elija
+int do_replace_number(char main_array[0][6], char replacer_1, char replacer_2) {
+    
+
+    for (int i=0; i<6; i++) {
+        for(int u=0; u<6; u++) {
+            if(main_array[i][u]==replacer_1) {
+                main_array[i][u]=replacer_2;
+            }
+        }
+    }
+
+    return 0;
+}
+
+// Esta funcion mueve la serpiente un bloque. Se puede invocar varias veces para que la serpiente se mueva una variable indenfinida de bloques
 int do_snake_movement(char main_array[][6]) {
     int temp_num, pos_1, pos_2, ant_pos_1, ant_pos_2;
 
-    // Busco la cabeza de la serpiente
-    for (int i=0; i<6; i++)  {
-        for (int u=0; u<6; u++)  {
+    // Esta parte del codigo busca la cabeza de la serpiente, la pone en una variable, y me hace un backup
+    for (int i=0; i<6; i++) 
+        for (int u=0; u<6; u++)
             if(main_array[i][u]=='0') {
                 pos_1=i;
                 pos_2=u;
             }
-        }
-    }
-    // Dejo guardada la posicion de la cabeza
-    ant_pos_1 = pos_1;
-    ant_pos_2 = pos_2;
+    ant_pos_1=pos_1;
+    ant_pos_2=pos_2;
 
-
-    //This while will look for empty cells. Once it finds one, It will put the symbol on it.
+    //Este while del codigo busca una posicion vecina valida para mover la cabeza de la serpiente y la mueve
     while(main_array[pos_1][pos_2] != ' ' ) { 
         temp_num=rand()%2;
         if (temp_num==0) { 
-            // If snake's head in the column cero or 5, move it to the right or to the left. If not in 0, do the same, but to any neighboring cell
             if(pos_1==0)
                 pos_1++;
             else if(pos_1==5)
@@ -51,7 +119,6 @@ int do_snake_movement(char main_array[][6]) {
                     pos_1--;
             }
         }
-        // In this case, It will move in between rows under that rules commented at the if
         else {
             if(pos_2==0)
                 pos_2++;
@@ -71,18 +138,19 @@ int do_snake_movement(char main_array[][6]) {
             pos_2 = ant_pos_2;
         }
     }
-    main_array[pos_1][pos_2]='0'; // Pongo la cabeza en la nueva posicion
+    main_array[pos_1][pos_2]='A'; // 
 
-    for (int i=0; i<6; i++)  { // Busco la cola de la serpiente, para eliminarla antes de moverla, así no se repite
-        for (int u=0; u<6; u++)  {
-            if(main_array[i][u]=='1') {
-                main_array[i][u]=' ';
-            }
+    // El numero de celdas de la serpiente, hace el codigo exponencialmente mas dificil, asi que hice un sistema de letras para hacerlo sencillo y escalable
+    // Reeplaza los valores en la siguiente posicion de la serpiente
+    // Los reeplaza por letras, asi el numero no se multiplica
+    // Luego los vuelve a convertir en numeros
+    do_replace_number(main_array, '0', 'B');
+    do_replace_number(main_array, '1', 'C');
+    do_replace_number(main_array, '2', ' ');
 
-        }
-    }
-
-    main_array[ant_pos_1][ant_pos_2]='1';
+    do_replace_number(main_array, 'A', '0');
+    do_replace_number(main_array, 'B', '1');
+    do_replace_number(main_array, 'C', '2');
     
     return 0;
 }
@@ -92,7 +160,7 @@ int do_snake_movement(char main_array[][6]) {
 int main() {
     srand (time(NULL));
     char main_array[6][6];
-    int pos_1, pos_2;
+    int pos_1, pos_2, movimientos;
 
     for (int i=0; i<6; i++)  { // Puts empty spaces in array
         for (int u=0; u<6; u++)  {
@@ -100,23 +168,18 @@ int main() {
         }
     }
 
-    // Defines started position for the snake's head
-    pos_1=rand()%6;   
-    pos_2=rand()%6;
-    main_array[pos_1][pos_2]='0';
+    //Spawnea las tres celdas originales de la serpiente. Van a estar en una posicion al azar
+    do_spawn_snake(main_array);
+    print_array(main_array);
 
-    //Moves snake two blocks
-    for (int u=0;u<4;u++) {
-        for(int i=0; i<2;i++) {
-            do_snake_movement(main_array);
+    movimientos = 5; // Este loop pone a voluntad del programador la cantidad de movimientos de tres que hara la serpiente
+    for (int i=0; i<movimientos; i++) {
+        for(int u=0; u<3; u++) {
+        do_snake_movement(main_array);
         }
         print_array(main_array);
     }
-
-
-    // Ahora tengo la serpiente en posicion.
-    
         
-
+    return 0;
 
 }
